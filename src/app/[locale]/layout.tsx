9f70ec13a42cc4ae1240727deb5db76662439355
Eslam-E-Hossni <eslam.e.hossni@gmail.com>
2025-03-26 14:31:57 +0400

    feat: improve SEO and meta tags
    - Update meta tags to improve SEO.
    - Add schema.org structured data for better search engine visibility.
    - Improve meta descriptions for both English and Arabic versions.
    - Remove redundant metadata exports.
    - Add more relevant keywords for better search results.

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -6,7 +6,7 @@ import { NextIntlClientProvider, hasLocale } from "next-intl";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import { IBM_Plex_Sans_Arabic, Chakra_Petch } from "next/font/google";
 import "@/styles/globals.css";
 import LandingHeader from "@/components/layout/landing-header";
@@ -25,11 +25,11 @@ const chakraPetch = Chakra_Petch({
   weight: ["300", "400", "500", "600", "700"],
 });
 
-export const metadata: Metadata = {
-  title: "احمي سيارتك بأعلى معايير الجودة",
-  description:
-    "aar-aarluxe احمي سيارتك بأعلى معايير الجودة مع خدماتنا الفاخرة. نقدم تلميع وتغطية سيارات بضمان حتى 7 سنوات. اكتشف أفضل خدمات العناية بالسيارات في.",
-};
+// export const metadata: Metadata = {
+//   title: "احمي سيارتك بأعلى معايير الجودة",
+//   description:
+//     "aar-aarluxe احمي سيارتك بأعلى معايير الجودة مع خدماتنا الفاخرة. نقدم تلميع وتغطية سيارات بضمان حتى 7 سنوات. اكتشف أفضل خدمات العناية بالسيارات في.",
+// };
 
 export default async function LocaleLayout({
   children,
@@ -46,42 +46,97 @@ export default async function LocaleLayout({
 
   return (
     <html dir={locale === "ar" ? "rtl" : "ltr"} lang={locale}>
-      <meta
-        name="keywords"
-        content="عناية بالسيارات, حماية السيارات, خدمات سيارات, تلميع سيارات, تغطية السيارات, ضمان حماية السيارات, أفضل خدمات السيارات, عناية فاخرة بالسيارات, حماية سيارات فاخرة, خدمات سيارات عالية الجودة, تلميع سيارات محترف, تغطية سيارات بضمان, عناية سيارات بأعلى معايير, حماية سيارات لمدة 7 سنوات"
-      />
-      <meta property="og:title" content="احمي سيارتك بأعلى معايير الجودة" />
-      <meta
-        property="og:description"
-        content="احمي سيارتك بأعلى معايير الجودة مع خدماتنا الفاخرة. نقدم تلميع وتغطية سيارات بضمان حتى 7 سنوات."
-      />
-      <meta
-        property="og:image"
-        content="https://aarluxe.online/assets/images/logo.svg"
-      />
-      <meta property="og:url" content="https://aarluxe.online" />
-      <meta
-        name="twitter:card"
-        content="https://aarluxe.online/assets/images/landing-bg-02.jpg"
-      />
-      <meta name="twitter:title" content="احمي سيارتك بأعلى معايير الجودة" />
-      <meta
-        property="og:site_name"
-        content="احمي سيارتك بأعلى معايير الجودة - aarluxe car care"
-      ></meta>
-      <meta
-        name="twitter:description"
-        content="احمي سيارتك بأعلى معايير الجودة مع خدماتنا الفاخرة. نقدم تلميع وتغطية سيارات بضمان حتى 7 سنوات."
-      />
-      <meta
-        name="twitter:image"
-        content="https://aarluxe.online/assets/images/landing-bg-02.jpg"
-      />
-      <meta
-        name="google-site-verification"
-        content="rZplCzD4MObzqAdvNbc1T1wm6QeQ7lLkVkYE403Amhc"
-      />
-      <link rel="canonical" href="https://aarluxe.online" />
+      <head>
+        {locale === "ar" ? (
+          <>
+            <title>احمي سيارتك بأعلى معايير الجودة | AAR LUXE CAR CARE</title>
+            <meta
+              name="twitter:title"
+              content="احمي سيارتك بأعلى معايير الجودة"
+            />
+            <meta
+              name="twitter:description"
+              content="احمي سيارتك بأعلى معايير الجودة مع خدماتنا الفاخرة التي تضم تلميع سيارات عالي الجودة، تغطية سيارات متطورة، وحماية شاملة ضد الأضرار والخدوش. نحن نقدم خدمات متخصصة في تلميع وتغطية السيارات الفاخرة مع ضمان طويل الأمد يصل إلى 7 سنوات، مما يضمن لك الحفاظ على مظهر سيارتك وحمايتها من العوامل الخارجية. بالإضافة إلى ذلك، نستخدم تقنيات متطورة لضمان حماية فعّالة ونتائج مذهلة، مما يجعل سيارتك تبدو جديدة دائماً. اختبر العناية الفائقة التي نقدمها مع أفضل خدمات صيانة وحماية السيارات لتلبية جميع احتياجاتك."
+            />
+            <meta
+              name="description"
+              content="احمي سيارتك بأعلى معايير الجودة مع خدماتنا الفاخرة التي تضم تلميع سيارات عالي الجودة، تغطية سيارات متطورة، وحماية شاملة ضد الأضرار والخدوش. نحن نقدم خدمات متخصصة في تلميع وتغطية السيارات الفاخرة مع ضمان طويل الأمد يصل إلى 7 سنوات، مما يضمن لك الحفاظ على مظهر سيارتك وحمايتها من العوامل الخارجية. بالإضافة إلى ذلك، نستخدم تقنيات متطورة لضمان حماية فعّالة ونتائج مذهلة، مما يجعل سيارتك تبدو جديدة دائماً. اختبر العناية الفائقة التي نقدمها مع أفضل خدمات صيانة وحماية السيارات لتلبية جميع احتياجاتك."
+            />
+
+            <meta
+              name="keywords"
+              content="عناية شاملة بالسيارات لجميع أنواع السيارات، خدمات حماية السيارات المتقدمة والتكنولوجية، أفضل خدمات العناية بالسيارات والصيانة الاحترافية، تلميع سيارات عالي الجودة لتحسين مظهر السيارة وحمايتها، تغطية السيارات الفاخرة لحمايتها من الأضرار، ضمان حماية سيارات شامل وممتد لعدة سنوات، أفضل خدمات العناية بالسيارات الفاخرة والمتطورة، حماية سيارات فاخرة بأحدث التقنيات وأعلى معايير الجودة، خدمات صيانة وتلميع سيارات محترفة للحفاظ على قيمتها، تغطية سيارات مضمونة لحمايتها من العوامل الجوية والاصطدامات، عناية فائقة بالسيارات باستخدام تقنيات حديثة وبأعلى معايير الأمان، حماية سيارات بمدة ضمان تصل إلى 7 سنوات ضد العوامل الخارجية، خدمات صيانة فاخرة لعناية شاملة بالسيارات بأعلى جودة، حماية السيارات الفاخرة من الخدوش والأضرار الخارجية مع ضمان طويل الأمد، أفضل خدمات تلميع وتغطية للسيارات الفاخرة للحفاظ على بريقها ومظهرها."
+            />
+            <meta
+              property="og:title"
+              content="احمي سيارتك بأعلى معايير الجودة"
+            />
+            <meta
+              property="og:description"
+              content="احمي سيارتك بأعلى معايير الجودة مع خدماتنا الفاخرة التي تضم تلميع سيارات عالي الجودة، تغطية سيارات متطورة، وحماية شاملة ضد الأضرار والخدوش. نحن نقدم خدمات متخصصة في تلميع وتغطية السيارات الفاخرة مع ضمان طويل الأمد يصل إلى 7 سنوات، مما يضمن لك الحفاظ على مظهر سيارتك وحمايتها من العوامل الخارجية. بالإضافة إلى ذلك، نستخدم تقنيات متطورة لضمان حماية فعّالة ونتائج مذهلة، مما يجعل سيارتك تبدو جديدة دائماً. اختبر العناية الفائقة التي نقدمها مع أفضل خدمات صيانة وحماية السيارات لتلبية جميع احتياجاتك."
+            />
+            <meta
+              property="og:site_name"
+              content="احمي سيارتك بأعلى معايير الجودة - aarluxe car care"
+            ></meta>
+          </>
+        ) : (
+          <>
+            <title>
+              Protect Your Car with the Highest Quality Standards | AAR LUXE CAR
+              CARE
+            </title>
+            <meta
+              name="twitter:title"
+              content="Protect Your Car with the Highest Quality Standards"
+            />
+            <meta
+              name="twitter:description"
+              content="Protect your car with our luxurious services, including high-quality car polishing, advanced car coverage, and comprehensive protection against damage and scratches. We offer specialized services in polishing and covering luxury cars with a long-term guarantee of up to 7 years, ensuring that your car's appearance is preserved and protected from external factors. Additionally, we use advanced techniques to ensure effective protection and amazing results, keeping your car looking like new. Experience the exceptional care we provide with the best car maintenance and protection services to meet all your needs."
+            />
+            <meta
+              name="description"
+              content="Protect your car with our luxurious services, including high-quality car polishing, advanced car coverage, and comprehensive protection against damage and scratches. We offer specialized services in polishing and covering luxury cars with a long-term guarantee of up to 7 years, ensuring that your car's appearance is preserved and protected from external factors. Additionally, we use advanced techniques to ensure effective protection and amazing results, keeping your car looking like new. Experience the exceptional care we provide with the best car maintenance and protection services to meet all your needs."
+            />
+
+            <meta
+              name="keywords"
+              content="aarluxe online, aar-luxe.online, aar-luxe online, comprehensive car care for all types of cars, advanced and technological car protection services, best car care and professional maintenance services, high-quality car polishing to improve appearance and protection, luxury car coverage to protect from damage, comprehensive car protection guarantee for several years, best luxury car care services with advanced technology, luxury car protection with the latest techniques and highest quality standards, professional car polishing and maintenance services to preserve value, guaranteed car coverage to protect from weather and impact, premium car care using modern techniques and the highest safety standards, car protection with a guarantee of up to 7 years against external factors, luxury car maintenance services for comprehensive care with top quality, luxury car protection from scratches and external damage with a long-term guarantee, best luxury car polishing and coverage services to maintain its shine and appearance."
+            />
+            <meta
+              property="og:title"
+              content="Protect Your Car with the Highest Quality Standards"
+            />
+            <meta
+              property="og:description"
+              content="Protect your car with our luxurious services, including high-quality car polishing, advanced car coverage, and comprehensive protection against damage and scratches. We offer specialized services in polishing and covering luxury cars with a long-term guarantee of up to 7 years, ensuring that your car's appearance is preserved and protected from external factors. Additionally, we use advanced techniques to ensure effective protection and amazing results, keeping your car looking like new. Experience the exceptional care we provide with the best car maintenance and protection services to meet all your needs."
+            />
+            <meta
+              property="og:site_name"
+              content="Protect Your Car with the Highest Quality Standards - AAR LUXE CAR CARE"
+            />
+          </>
+        )}
+        <meta name="robots" content="index, follow" />
+        <meta
+          property="og:image"
+          content="https://www.aarluxe.online/assets/images/logo.svg"
+        />
+        <meta property="og:url" content="https://www.aarluxe.online" />
+        <meta
+          name="twitter:image"
+          content="https://www.aarluxe.online/assets/images/logo.svg"
+        />
+        <meta
+          name="twitter:card"
+          content="https://www.aarluxe.online/assets/images/landing-bg-02.jpg"
+        />
+        <link rel="canonical" href="https://www.aarluxe.online" />
+        <meta
+          name="google-site-verification"
+          content="rZplCzD4MObzqAdvNbc1T1wm6QeQ7lLkVkYE403Amhc"
+        />
+      </head>
       <body
         className={`${ibmPlexSansArabic.variable} ${
           chakraPetch.variable
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,6 +1,6 @@
 import HeroSection from "@/components/landing-sections/hero-section";
 import AboutUsSection from "@/components/landing-sections/about-us-sections";
-import { Metadata } from "next";
+// import { Metadata } from "next";
 import { hasLocale } from "next-intl";
 import WhyUsSection from "@/components/landing-sections/why-us-section";
 import ServicesSection from "@/components/landing-sections/services-section";
@@ -9,12 +9,13 @@ import GellarySection from "@/components/landing-sections/gallery-section";
 import ContactSection from "@/components/landing-sections/contact-section";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
+import Head from "next/head";
 
-export const metadata: Metadata = {
-  title: "احمي سيارتك بأعلى معايير الجودة | AAR LUXE CAR CARE",
-  description:
-    "نقدم لك تجربة فريدة في عالم العناية بالسيارات حيث الجودة والاحترافية تلتقيان لتوفير أفضل الخدمات لسيارتك بأفضل الخامات وبضمان حتى 7 سنوات",
-};
+// export const metadata: Metadata = {
+//   title: "احمي سيارتك بأعلى معايير الجودة | AAR LUXE CAR CARE",
+//   description:
+//     "نقدم لك تجربة فريدة في عالم العناية بالسيارات حيث الجودة والاحترافية تلتقيان لتوفير أفضل الخدمات لسيارتك بأفضل الخامات وبضمان حتى 7 سنوات",
+// };
 
 // Define the supported locales
 const locales = ["en", "ar"];
@@ -36,14 +37,30 @@ export default async function Home({
   }
 
   return (
-    <div className="">
-      <HeroSection locale={locale} />
-      <AboutUsSection locale={locale} />
-      <WhyUsSection locale={locale} />
-      <ServicesSection locale={locale} />
-      <GellarySection />
-      <PartnersSection />
-      <ContactSection locale={locale} />
-    </div>
+    <>
+      <Head>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "http://schema.org",
+              "@type": "WebSite",
+              url: "https://www.aarluxe.online",
+              name: "AarLuxe",
+              description: "Your description here",
+            }),
+          }}
+        />
+      </Head>
+      <div className="">
+        <HeroSection locale={locale} />
+        <AboutUsSection locale={locale} />
+        <WhyUsSection locale={locale} />
+        <ServicesSection locale={locale} />
+        <GellarySection />
+        <PartnersSection />
+        <ContactSection locale={locale} />
+      </div>
+    </>
   );
 }
