8b733e4166c8a21f99e32bd35df917bde7fcd9d1
Eslam-E-Hossni <eslam.e.hossni@gmail.com>
2025-03-30 01:19:58 +0400

    style: improve hero section visuals
    - Updated the styling of the hero section to improve its visual appeal.
    - Removed the `bg-fixed` class from the hero section.
    - Added responsiveness to the `SmokeScene` component, making it only visible on medium and larger screens.
    - Reduced opacity of the overlay on smaller screens.

--- src/components/landing-sections/hero-section.tsx
+++ src/components/landing-sections/hero-section.tsx
@@ -8,12 +8,14 @@ const HeroSection = async ({ locale }: { locale: string }) => {
   const t = await getTranslations("components.HeroSection");
   return (
     <section
-      className="bg-[image:var(--hero-bg)] bg-no-repeat h-screen w-full bg-cover bg-fixed min-h-screen relative overflow-x-hidden"
+      className="bg-[image:var(--hero-bg)] bg-no-repeat h-screen w-full bg-cover min-h-screen relative overflow-x-hidden"
       id="hero-section"
     >
       <div className="smoke-effect absolute top-0 left-0 w-full h-full z-0">
-        <SmokeScene />
-        <span className="absolute left-0 top-0 w-full h-full bg-black opacity-60"></span>
+        <div className="hidden md:block absolute inset-0">
+          <SmokeScene />
+        </div>
+        <span className="absolute left-0 top-0 w-full h-full bg-black opacity-30 md:opacity-60"></span>
       </div>
       <div className="container">
         <div className="flex items-center min-h-screen overflow-hidden">
